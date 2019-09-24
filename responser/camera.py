877ae42468e8f31871e0ed98c70aec03c15e2b30
hans0406 <hans0406tammy@gmail.com>
2019-09-24 08:55:49 +0800

    use contex managment, close picamera if error image flip vertical

--- responser/camera.py
+++ responser/camera.py
@@ -8,8 +8,11 @@ TMP_IMAGE_TYPE = 'jpeg'
 
 def response_picture(receipter_id):
     response.response_text(receipter_id, "taking picture...")
-    camera = picamera.PiCamera()
-    camera.resolution = (2592, 1944)
-    camera.capture(TMP_IMAGE)
-    camera.close()
+    try:
+        camera = picamera.PiCamera()
+        camera.vflip = true
+        camera.resolution = (2592, 1944)
+        camera.capture(TMP_IMAGE)
+    finally:
+        camera.close()
     response.response_image(receipter_id, TMP_IMAGE, TMP_IMAGE_TYPE)
