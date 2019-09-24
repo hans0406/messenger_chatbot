47be3d8f0d8fbd894664ff3aee58feb186daa820
hans0406 <hans0406tammy@gmail.com>
2019-09-24 08:48:37 +0800

    fix servo setting error set servo move range 22.5 to 135 degree

--- responser/servomotor.py
+++ responser/servomotor.py
@@ -6,25 +6,24 @@ import RPi.GPIO as GPIO
 import time
 
 SERVO = 26
-ANGLE = 2.5
+STEP = 9.5/8
+SERVO_ANGLE = (2.5 + 12) / 2
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SERVO, GPIO.OUT)
 SG90 = GPIO.PWM(SERVO, 50)
-SG90.start(ANGLE)
+SG90.start(0)
 
 def turn_left():
-    global ANGLE
-    if ANGLE < 6:
-        ANGLE += 0.5
-        SG90.ChangeDutyCycle(ANGLE)
-        time.spleep(0.02)
-        SG90.ChangeDutyCycle(0)
+    global SERVO_ANGLE
+    if SERVO_ANGLE < 12 - (STEP*2):
+        SERVO_ANGLE += STEP
+        SG90.ChangeDutyCycle(SERVO_ANGLE)
+        print("turn left", SERVO_ANGLE)
 
 def turn_right():
-    global ANGLE
-    if ANGLE > 3:
-        ANGLE -= 0.5
-        SG90.ChangeDutyCycle(ANGLE)
-        time.spleep(0.02)
-        SG90.ChangeDutyCycle(0)
+    global SERVO_ANGLE
+    if SERVO_ANGLE > 2.5 + STEP:
+        SERVO_ANGLE -= STEP
+        SG90.ChangeDutyCycle(SERVO_ANGLE)
+        print("turn right", SERVO_ANGLE)
 
